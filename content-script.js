94e6284c07622afc3e64865c0d758aebecb4d1f9
xxuxa-k <tk0x2bc@gmail.com>
2022-05-09 23:30:52 +0900

    setTimeoutの再帰でボタンを監視

--- content-script.js
+++ content-script.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', e => {
+  disableSendButton()
+})
+
+function disableSendButton() {
   const candidates = document.querySelectorAll('div[role="button"][aria-label^="送信"]');
-  if (candidates.length === 1) {
+  if (candidates && candidates.length === 1) {
     const button = candidates.item(0);
     button.style["pointerEvents"] = 'none';
   }
-})
+  setTimeout(disableSendButton, 500)
+}
+
